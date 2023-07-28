ece227fc6c419dd87555f7cda5ea5975dd5e92b1
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-28 12:54:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function volume_sphere() {
+function volume_sphere(e) {
+	e.preventDefault();
     const radi = document.getElementById('radius');
 	const r = parseInt(radi.value);
 	console.log(r);
+
+	const volume = 4 * 3.14 * Math.pow(r,3) / 3;
+	document.getElementById('volume').value = volume;
   
 } 
 
